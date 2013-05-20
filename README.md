3a87dbd9ccf25ddc46961f905acccd85494769e7
Paul Knox-Kennedy <paul@knox-kennedy.me.uk>
2013-05-20 13:33:07 +0100

    Moved lots of bits around to make this a module

---
+++ README.md
@@ -0,0 +1,9 @@
+Roster
+=========
+
+Rostering Management web app using Python Flask
+
+Introduction
+------------
+
+A quick and dirty rostering app using python flask
---
+++ runserver.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+from kemosahbi import app
+import kemosahbi.models 
+
+import sys, getopt
+
+def main(argv):
+    _init_db = False
+    _add_test_data = False
+    try:
+        opts, args = getopt.getopt(argv,"h",["init-db","init-test-db"])
+    except getopt.GetoptError:
+        print 'runserver.py [--init-db | --init-test-db] '
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'runserver.py [--init-db | --init-test-db]'
+            sys.exit()
+        elif opt in ("--init-db"):
+            _init_db = True
+        elif opt in ("--init-test-db"):
+            _init_db = True
+            _add_test_data = True
+    if _init_db:
+        kemosahbi.models.init_db()
+        sys.exit()
+    if _add_test_data:
+        kemosahbi.models.init_db()
+        kemosahbi.models.add_test_data()
+        sys.exit()
+    app.run(host='0.0.0.0',debug=True)
+   
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+
